74ef83051860eb90e9a8de1210173421ed0476e0
Zach Dubno <zach@Zach-Dubnos-MacBook-Pro.local>
2017-04-29 15:21:56 -0400

    finished lab

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  new_array = []
+  if block_given?
+    i = 0
+    while i < array.length
+      new_array << yield(array[i])
+      i += 1
+    end
+  else
+    puts "No block given :("
+  end
+  new_array
+end
